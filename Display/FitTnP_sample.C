ce4fad602b19c6b8d33f390b7db3135f93bc45f3
KiSoo <kilee@cern.ch>
2014-08-16 17:14:11 +0200

    FitTnP_sample.C on modifying

--- Display/FitTnP_sample.C
+++ Display/FitTnP_sample.C
@@ -38,33 +38,55 @@ void FitTnP_sample(){
   double mumutrklow=2.;
   double mumutrkhigh=5.;
   
-  RooRealVar mass("mass", "mass", mumulow, mumuhigh);
-  RooRealVar masstrk("masstrk", "masstrk", mumutrklow, mumutrkhigh);
+  RooRealVar mass("mass", "mass", mumutrklow, mumutrkhigh);
+  //RooRealVar masstrk("masstrk", "masstrk", mumutrklow, mumutrkhigh);
   mass.setBins(100);
 
- // const double MuPtBin[nMuPtBin+1] = {0.0, 1.5, 3.0, 4.5, 6.0, 9.0, 20.0, 30.0};
+  //const double MuPtBin[nMuPtBin+1] = {0.0, 1.5, 3.0, 4.5, 6.0, 9.0, 20.0, 30.0};
   //const double MuEtaBin[nMuEtaBin+1] = {-2.4, -1.5, -0.5, 0.5, 1.5, 2.4};
+
+  //pt histogram
+  //TH1D* hTrkPtPass[nMuPtBin];
+  //TH1D* hTrkPtFail[nMuPtBin];
+  //TH1D* hTrkPtAll[nMuPtBin];
+  TH1D* hTrkPtPass;
+  TH1D* hTrkPtFail;
+  TH1D* hTrkPtAll;
+  /*
+  TH1D* hTrkEtaPass[nMuEtaBin];
+  TH1D* hTrkEtaFail[nMuEtaBin];
+  TH1D* hTrkEtaAll[nMuEtaBin];
+*/
+  TFile*finput=new TFile("foutput.root","read");
+  finput->cd();
 
   RooCategory sampleTrk("sampleTrk", "sampleTrk");
   sampleTrk.defineType("Pass");
   sampleTrk.defineType("Fail");
-  //sampleTrk.defineType("All");
 
   TCanvas* cpass = new TCanvas("cpass", "", 0, 20, 600, 600);
   TCanvas* cfail = new TCanvas("cfail", "", 0, 40, 600, 600);
-  TCanvas* call;
+  TCanvas* call = new TCanvas("call", "", 0, 60, 600, 600);
 
-  //pt histogram
-  TH1D* hTrkPtPass;
-  TH1D* hTrkPtFail;
-  TH1D* hTrkPtAll;
-  
-  TFile*finput=new TFile("foutput.root","read");
-  finput->cd();
+//  for(int i = 0; i < nMuPtBin; i++)
+//  {
+//    hTrkPtPass[i] = (TH1D*)finput->Get(Form("hTrkPtPass%d"), i);
+//    hTrkPtFail[i] = (TH1D*)finput->Get(Form("hTrkPtFail%d"), i);
+//    hTrkPtFail[i] = (TH1D*)finput->Get(Form("hTrkPtFail%d"), i);
 
+    hTrkPtPass = (TH1D*)finput->Get("hTrkPtPass3");
+    hTrkPtFail = (TH1D*)finput->Get("hTrkPtFail3");
+    hTrkPtAll = (TH1D*)finput->Get("hTrkPtAll3");
+//  }
+/*
+  for(int i = 0; i < nMuEtaBin; i++)
+  {
+    hTrkEtaPass[i] = (TH1D*)finput->Get(Form("hTrkEtaPass%d"), i);
+    hTrkEtaFail[i] = (TH1D*)finput->Get(Form("hTrkEtaFail%d"), i);
+    hTrkEtaFail[i] = (TH1D*)finput->Get(Form("hTrkEtaFail%d"), i);
+  }
+*/
   //get pt histogram
-  hTrkPtPass = (TH1D*)finput->Get("hTrkPtPass3");
-  hTrkPtFail = (TH1D*)finput->Get("hTrkPtFail3");
   //hTrkPtAll = (TH1D*)finput->Get("hTrkPtAll3");
 
   //make CB function
@@ -76,7 +98,6 @@ void FitTnP_sample(){
 
   RooCBShape signal_CB_Pass("signal_CB_Pass", "signal_CB_Pass", mass, mean_CB, sigma_CB, alpha_CB, n_CB);
   RooCBShape signal_CB_Fail("signal_CB_Fail", "signal_CB_Fail", mass, mean_CB, sigma_CB, alpha_CB, n_CB);
-  //RooCBShape signal_CB_All("signal_CB_All", "signal_CB_sAll", mass, mean_CB, sigma_CB, alpha_CB, n_CB);
 
   //make 2gauss function
   RooRealVar mean_gaus("mean_gaus", "mean_gaus", 3.1, 3.0, 3.2);
@@ -88,7 +109,6 @@ void FitTnP_sample(){
 
   RooAddPdf signal_gauss_Pass("signal_gauss_Pass", "signal_gauss_Pass", RooArgList(gauss1, gauss2), RooArgList(mfrac_gaus));
   RooAddPdf signal_gauss_Fail("signal_gauss_Fail", "signal_gauss_Fail", RooArgList(gauss1, gauss2), RooArgList(mfrac_gaus));
-  //RooAddPdf signal_gauss_All("signal_gauss_All", "signal_gauss_All", RooArgList(gauss1, gauss2), RooArgList(mfrac_gaus));
 
   //make Chebyschev function
   RooRealVar cheb_p1Pass("cheb_paPass", "cheb_p1Pass", 0., -1., +1.);
@@ -97,26 +117,34 @@ void FitTnP_sample(){
   RooRealVar cheb_p1Fail("cheb_p1Fail", "cheb_p1Fail", 0., -1., +1.);
   RooRealVar cheb_p2Fail("cheb_p2Fail", "cheb_p2Fail", 0., -1., +1.);
   RooChebychev background_cheb_Fail("background_cheb_Fail", "background_cheb_Fail", mass, RooArgList(cheb_p1Fail, cheb_p2Fail));
-  //RooRealVar cheb_p1All("cheb_p1All", "cheb_p1All", 0., -1., +1.);
-  //RooRealVar cheb_p2All("cheb_p2All", "cheb_p2All", 0., -1., +1.);
-  //RooChebychev background_cheb_All("background_cheb_All", "background_cheb_All", mass, RooArgList(cheb_p1All, cheb_p2All));
 
   //make exponential function
   RooRealVar lp("lp", "lp", 0.0, -5., 5.);
   RooRealVar lf("lf", "lf", 0.0, -5., 5.);
-  //RooRealVar la("la", "la", 0.0, -5., 5.);
   RooExponential background_exp_Pass("background_exp_Pass", "background_exp_Pass", mass, lp);
   RooExponential background_exp_Fail("background_exp_Fail", "background_exp_Fail", mass, lf);
-  //RooExponential background_exp_All("background_exp_All", "background_exp_All", mass, la);
 
   //set axis variable
-  RooRealVar xtrk("xtrk", "mass", mumutrklow, mumutrkhigh, "GeV");
   RooRealVar weight("weight", "weight", 1., 0, 1E9);
 
-
+//  for(int i = 0; i < nMuPtBin; i ++)
+//  {
+    //RooDataHist dataTrkPass("dataTrkPass", "dataTrkPass", mass, hTrkPtPass[i]);
+    //RooDataHist dataTrkFail("dataTrkFail", "dataTrkFail", mass, hTrkPtFail[i]);
+    //RooDataHist dataTrkAll("dataTrkAll", "dataTrkFAll", mass, hTrkPtAll[i]);
+    RooDataHist dataTrkPass("dataTrkPass", "dataTrkPass", mass, hTrkPtPass);
+    RooDataHist dataTrkFail("dataTrkFail", "dataTrkFail", mass, hTrkPtFail);
+    RooDataHist dataTrkAll("dataTrkAll", "dataTrkFAll", mass, hTrkPtAll);
+//  }
+/*
+  for(int i = 0; i < nMuEtaBin; i++)
+  {
+    RooDataHist dataTrkPass("dataTrkPass", "dataTrkPass", mass, hTrkEtaPass[i]);
+    RooDataHist dataTrkFail("dataTrkFail", "dataTrkFail", mass, hTrkEtaFail[i]);
+    RooDataHist dataTrkAll("dataTrkAll", "dataTrkAll", mass, hTrkEtaAll[i]);
+  }
+*/
   //set histogram
-  RooDataHist dataTrkPass("dataTrkPass", "dataTrkPass", mass, hTrkPtPass);
-  RooDataHist dataTrkFail("dataTrkFail", "dataTrkFail", mass, hTrkPtFail);
   //RooDataHist dataTrkAll("dataTrkAll", "dataTrkAll", xtrk, hTrkPtAll);
 
   RooDataSet dataTrkPassset("dataTrkPassset", "mass", RooArgSet(mass, weight), WeightVar("weight"));
@@ -138,12 +166,13 @@ void FitTnP_sample(){
   RooRealVar nbkgFail("nbkgFail", "nbkgFail", 1, 0, 1E9);
 
   //define PDF
+/*
   RooAddPdf model_CB_cheb_Pass("model_CB_cheb_Pass", "model_CB_cheb_Pass", RooArgList(signal_CB_Pass, background_cheb_Pass), RooArgList(nsigPass, nbkgPass));
   RooAddPdf model_CB_cheb_Fail("model_CB_cheb_Fail", "model_CB_cheb_Fail", RooArgList(signal_CB_Fail, background_cheb_Fail), RooArgList(nsigFail, nbkgFail));
 
   RooAddPdf model_CB_exp_Pass("model_CB_exp_Pass", "model_CB_exp_Pass", RooArgList(signal_CB_Pass, background_exp_Pass), RooArgList(nsigPass, nbkgPass));
   RooAddPdf model_CB_exp_Fail("model_CB_exp_Fail", "model_CB_exp_Fail", RooArgList(signal_CB_Fail, background_exp_Fail), RooArgList(nsigFail, nbkgFail));
-
+*/
   RooAddPdf model_gaus_cheb_Pass("model_gaus_cheb_Pass", "model_gaus_cheb_Pass", RooArgList(signal_gauss_Pass, background_cheb_Pass), RooArgList(nsigPass, nbkgPass));
   RooAddPdf model_gaus_cheb_Fail("model_gaus_cheb_Fail", "model_gaus_cheb_Fail", RooArgList(signal_gauss_Fail, background_cheb_Fail), RooArgList(nsigFail, nbkgFail));
 
@@ -157,8 +186,9 @@ void FitTnP_sample(){
   simPdf.fitTo(dataTrkAllset, Extended(true), NumCPU(numCPU), PrintLevel(quiet?-1:1), Warnings(!quiet));
 
   RooPlot *frame1 = mass.frame();
-  dataTrkAllset.plotOn(frame1, Cut("sampleTrk==sampleTrk::Pass"), Binning(100));
+  dataTrkAllset.plotOn(frame1, Cut("sampleTrk==sampleTrk::Pass"));
 
+  simPdf.plotOn(frame1, Slice(sampleTrk, "Pass"), ProjWData(sampleTrk,dataTrkAllset),LineStyle(kDashed), LineColor(kBlue));
   simPdf.plotOn(frame1, Slice(sampleTrk, "Pass"), Components("signal_gauss_Pass"),ProjWData(sampleTrk,dataTrkAllset),LineStyle(kDashed), LineColor(kGreen));
   simPdf.plotOn(frame1, Slice(sampleTrk, "Pass"), Components("background_cheb_Pass"),ProjWData(sampleTrk,dataTrkAllset),LineStyle(kDashed), LineColor(kRed));
 
@@ -166,22 +196,25 @@ void FitTnP_sample(){
   frame1->Draw();
 
   RooPlot *frame2 = mass.frame();
-  dataTrkFail.plotOn(frame2, Binning(100));
+  dataTrkAllset.plotOn(frame2, Cut("sampleTrk==sampleTrk::Fail"));
 
-  simPdf.plotOn(frame2, Slice(sampleTrk, "Fail"), Components("signal_gaus_Fail"),ProjWData(sampleTrk,dataTrkFail),LineStyle(kDashed), LineColor(kGreen));
-  simPdf.plotOn(frame2, Slice(sampleTrk, "Fail"), Components("background_cheb_Fail"),ProjWData(sampleTrk,dataTrkFail),LineStyle(kDashed), LineColor(kRed));
+  simPdf.plotOn(frame2, Slice(sampleTrk, "Fail"), ProjWData(sampleTrk,dataTrkAllset),LineStyle(kDashed), LineColor(kBlue));
+  simPdf.plotOn(frame2, Slice(sampleTrk, "Fail"), Components("signal_gauss_Fail"),ProjWData(sampleTrk,dataTrkAllset),LineStyle(kDashed), LineColor(kGreen));
+  simPdf.plotOn(frame2, Slice(sampleTrk, "Fail"), Components("background_cheb_Fail"),ProjWData(sampleTrk,dataTrkAllset),LineStyle(kDashed), LineColor(kRed));
 
   cfail->cd();
   frame2->Draw();
 
-  //RooPlot *frame3 = mass.frame();
-  //dataTrkAll.plotOn(frame3, Binning(100));
+  RooPlot *frame3 = mass.frame();
+  dataTrkAllset.plotOn(frame3);
 
-  //simPdf.plotOn(frame3, Slice(sampleTrk, "All"), Components("signal_gaus_All"),ProjWData(sampleTrk,dataTrkAll),LineStyle(kDashed), LineColor(kGreen));
-  //simPdf.plotOn(frame3, Slice(sampleTrk, "All"), Components("background_cheb_All"),ProjWData(sampleTrk,dataTrkAll),LineStyle(kDashed), LineColor(kRed));
+  simPdf.plotOn(frame3, ProjWData(sampleTrk, dataTrkAllset), LineColor(kGreen));
+  simPdf.plotOn(frame3, Components("signal_gauss_Pass, signal_gauss_Fail"), ProjWData(sampleTrk,dataTrkAllset),LineStyle(kDashed), LineColor(kGreen));
+//  simPdf.plotOn(frame3, ProjWData(sampleTrk,dataTrkAllset),LineStyle(kDashed), LineColor(kRed));
 
-  //call->cd();
-  //frame3->Draw();
+  call->cd();
+  frame3->Draw();
+
 }
 
 
